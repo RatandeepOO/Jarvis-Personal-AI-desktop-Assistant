387c4bfc0819e9e5d064ec8e9891914e08bc8d50
RatandeepOO <ratandeepdev@gmail.com>
2025-01-01 14:24:00 +0530

    ok

--- FINAL/what.py
+++ FINAL/what.py
@@ -1,20 +1,36 @@
-import pywhatkit as what
+import pywhatkit
 
-def sendmessage(recip_no, message, hour, minute):
+def sendmessage(recip_no, message):
     try:
-        what.sendwhatmsg(recip_no, message, hour, minute)
+        pywhatkit.sendwhatmsg_instantly(recip_no , message)
+        
+        (recip_no, message)
         print("Message sent successfully!")
     except Exception as e:
         print(f"An error occurred: {e}")
         
-def messageinput():
-    for i in len(list_no):
+def messageinput1(list_no):
+    
+    # Collect inputs from the user
+    name = input("Enter the name of the recipient: ")
+    recip_no = str(list_no)
+    message = input("Enter the message: ")
+    sendmessage(recip_no, message)
+        
+def messageinput(list_no):
+    for i in range(len(list_no)):  # Corrected loop
         # Collect inputs from the user
         name = input("Enter the name of the recipient: ")
         recip_no = str(list_no[i])
-        hour = int(input("Enter the hour (24-hour format): "))
-        minute = int(input("Enter the minutes: "))
         message = input("Enter the message: ")
-        sendmessage(recip_no, message, hour, minute)
+        sendmessage(recip_no, message)
 
-list_no = eval(input("Enter the number and seprate by commas (e.g.  XXXXXXXXXX , XXXXXXXXXX)"))
+no = int(input("Enter number of contacts ::"))
+if no > 1:
+    list_no = input("Enter the numbers and separate by commas (e.g. +XXXXXXXXXX, +XXXXXXXXXX) :: ").split(',')
+    messageinput(list_no)
+else:
+    list_no = [input("Enter the number:: ")]
+    messageinput1(list_no)
+
+print(list_no)
